96867f4d4aa8fe91e8dc5fd25ecfbc4d2e5d6eeb
abhishe99 <abhitomar2978@gmail.com>
2026-07-22 21:09:36 +0530

    Connect frontend and backend

--- Frontend/src/context/SocketContext.jsx
+++ Frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketProvider = ({ children }) => {
 
   useEffect(() => {
     if (authUser) {
-      const socket = io("http://localhost:4002", {
+      const socket = io(import.meta.env.VITE_API_URL, {
         query: {
           userId: authUser.user._id,
         },
